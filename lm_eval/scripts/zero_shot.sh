7fb557932d05d14c94ed0c88657551513a03bc63
Hasuer <2549109996@qq.com>
2024-08-08 10:17:04 +0800

    add eval scripts

---
+++ lm_eval/scripts/zero_shot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+GPU_ID=0,1,2,3,4,5,6,7
+
+# Set CUDA to use the specified GPU
+export CUDA_VISIBLE_DEVICES=$GPU_ID
+
+# 定义模型名称到模型路径的映射
+declare -A model_paths
+model_paths["model_name"]=model_path
+
+# Loop through each model and run zeroshot and fewshot
+for model_name in "${!model_paths[@]}"; do
+    echo ">>> Running zeroshot for $model_name"
+    lm_eval \
+        --model hf \
+        --model_args pretrained=${model_paths[$model_name]},trust_remote_code=True,parallelize=True \
+        --include_path ./ \
+        --tasks cmoraleval \
+        --output ./output/$model_name/zeroshot \
+        --log_samples \
+        --batch_size 16 #可根据计算资源自行修改
+done
